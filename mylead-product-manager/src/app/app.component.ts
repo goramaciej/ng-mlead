dc1790a23d5b43ffaa5d9aae70d1994055cc987e
maciejg <maciej.gora01@gmail.com>
2019-11-22 22:32:59 +0100

    add firebase support

--- mylead-product-manager/src/app/app.component.ts
+++ mylead-product-manager/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'mylead-product-manager';
+  productsCollection: AngularFirestoreCollection<Product>;
+  products: Observable<Product[]>;
+
+  name: string;
+  description: string;
+
+  constructor(private fireStore: AngularFirestore) {}
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+    this.productsCollection = this.fireStore.collection('products');
+    this.products = this.productsCollection.valueChanges();
+  }
+  async addProduct() {
+    // this.fireStor.collection('products').add({'name': this.name, 'description': this.description});
+
+    const per = this.fireStore.collection('products').doc('my-custom-id').set({'name': this.name, 'description': this.description})
+      .then(() => console.log('product added'))
+      .catch(() => console.log('problem'))
+      .then(() => console.log(per));
+  }
 }
--- mylead-product-manager/src/app/app.module.ts
+++ mylead-product-manager/src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJWN3DywIZ1H9b9oNpar_oHeUiqqw28-4",
+  authDomain: "ng-mylead-products.firebaseapp.com",
+  databaseURL: "https://ng-mylead-products.firebaseio.com",
+  projectId: "ng-mylead-products",
+  storageBucket: "ng-mylead-products.appspot.com",
+  messagingSenderId: "958129577292",
+  appId: "1:958129577292:web:370c41a074b72865d603e9",
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +25,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
