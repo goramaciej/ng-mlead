79217a8872ccb9930e7ad86d2dac5f3541e1874e
maciejg <maciej.gora01@gmail.com>
2019-11-27 11:30:49 +0100

    sorting products

--- mylead-product-manager/src/app/app.component.ts
+++ mylead-product-manager/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { ReactiveFormsModule } from '@angular/forms';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
@@ -20,15 +20,13 @@ import { Product } from './products/shared/product.model';
 
 export class AppComponent {
   title = 'mylead-product-manager';
+
   productsCollection: AngularFirestoreCollection<Product>;
   // products: Observable<Product[]>;
   products: any;
-
   productsDoc: AngularFirestoreDocument<Product>;
   selectedProduct: Observable<Product>;
-
   newEditProduct: Product = null;
-
 
   constructor(private fireStore: AngularFirestore) {}
   // tslint:disable-next-line: use-lifecycle-interface
@@ -39,23 +37,52 @@ export class AppComponent {
     // this.productsCollection = this.firestore.collection('products', ref => {
     //   ref.where('title', '==', 'product');
     // })
+    this.processData();
+    this.showData();
+  }
+
+  showData() {
+    let mapped = this.products.map( actions => {
+      console.log(actions);
+      return actions;
+    });
+  }
+
+  processData(filter: string = null, sortType: string = 'name') {
     this.products = this.productsCollection.snapshotChanges().map( actions => {
-      return actions.map( item => {
+      let mappedArr =  actions.map( item => {
         /*console.table(item.payload.doc.data());
         console.log(item.payload.doc.id);*/
         const productData = item.payload.doc.data() as Product;
         const id = item.payload.doc.id;
         return { id, productData };
       });
+      if (filter) {
+        const filteredArr = mappedArr.filter( item => item.productData.name.includes(filter) );
+        mappedArr = filteredArr;
+      }
+      return this.sortArray(mappedArr, sortType);
     });
   }
 
-  editProduct(productId) {
-    this.productsDoc = this.productsCollection.doc(productId);
+  sortArray(arr, sortType) {
+    switch (sortType) {
+      case 'price':
+          return arr.sort((a, b) => (a.productData.price > b.productData.price) ? 1 : -1);
+      case 'price-desc':
+          return arr.sort((a, b) => (a.productData.price < b.productData.price) ? 1 : -1);
+      default: // default sort by name
+          return arr.sort((a, b) => (a.productData.name > b.productData.name) ? 1 : -1);
+    }
+  }
+
+  editProductEv(productId) {
+    this.productsDoc = this.productsCollection.doc(productId.toString());
     this.selectedProduct = this.productsDoc.valueChanges();
     const dep = this.selectedProduct.pipe(take(1)).subscribe(value => {
       this.newEditProduct = value;
     });
+    this.blockScroll();
   }
 
   getProduct(productId) {
@@ -63,8 +90,8 @@ export class AppComponent {
     this.selectedProduct = this.productsDoc.valueChanges();
   }
 
-  deleteProduct(productId) {
-    this.productsCollection.doc(productId).delete().then( () => console.log('deleted'));
+  deleteProductEv(productId) {
+    this.productsCollection.doc(productId.toString()).delete().then( () => console.log('deleted'));
   }
   addNewProduct() {
     this.newEditProduct = new Product();
@@ -72,6 +99,7 @@ export class AppComponent {
     this.newEditProduct.id = Math.floor(Math.random() * 10000000);
   }
   addCommited(ev) {
+    console.log('addCommited');
     const per = this.productsCollection.doc(this.newEditProduct.id.toString()).set({
       name: this.newEditProduct.name,
       id: this.newEditProduct.id,
@@ -81,9 +109,33 @@ export class AppComponent {
     }).then( () => {
       // console.log('product added');
       this.newEditProduct = null;
+      this.unBlockScroll();
     });
   }
   addAbandoned(ev) {
     this.newEditProduct = null;
+    this.unBlockScroll();
   }
+
+  blockScroll() {
+    const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+    console.log(scrollY);
+    const body = document.body;
+    body.style.position = 'fixed';
+    body.style.top = `-${scrollY}`;
+  }
+  unBlockScroll() {
+    const body = document.body;
+    body.style.position = '';
+    body.style.top = '';
+    window.scrollTo(0, 250 * -1);
+    // document.getElementById('dialog').classList.remove('show');
+  }
+  filterMe() {
+    this.processData(null, 'price');
+  }
+
 }
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
--- mylead-product-manager/src/app/products/product-item/product-item.component.ts
+++ mylead-product-manager/src/app/products/product-item/product-item.component.ts
@@ -1,24 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../shared/product.model';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
-export class ProductItemComponent implements OnInit {
+export class ProductItemComponent {
 
   @Input()
-  product: any;
+  product: Product;
 
-  constructor() {
+  @Input()
+  adminMode = true;
 
-  }
+  @Output()
+  deleteProductEv: EventEmitter<number> = new EventEmitter<number>();
 
-  ngOnInit() {
+  @Output()
+  editProductEv: EventEmitter<number> = new EventEmitter<number>();
 
-  }
   logProduct() {
-    console.log(this.product);
+    console.table(this.product.id);
   }
 
+  format(what: number): string {
+    return what.toFixed(2).toString().replace('.', ',');
+  }
+
+  editProduct() {
+    this.editProductEv.emit(this.product.id);
+  }
+
+  deleteProduct() {
+    this.deleteProductEv.emit(this.product.id);
+  }
 }
